13b41d3e960fe42da33c1bbd961aa1887dce31a2
Chris Timperley <christimperley@googlemail.com>
2016-05-14 23:25:37 +0100

    Added vim colours to install.sh

--- install.sh
+++ install.sh
@@ -24,4 +24,5 @@ mkdir -p ~/.vim/bundle
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then 
   git clone -q https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+link vim/colors
 vim +PluginInstall +qall
