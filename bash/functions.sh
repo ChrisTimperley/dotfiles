880f1c063bdb2a03a09070f59a466ee1a569b1bd
Chris Timperley <christimperley@googlemail.com>
2016-12-18 02:03:15 -0500

    finished implementing bash module; loving it

--- bash/functions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Defines a bunch of useful functions
-function is_ubuntu {
-  lsb_release -a |& grep -q "Ubuntu"
-}
--- bash/ubuntu.sh
+++
@@ -1,7 +0,0 @@
-# Ensure we're running Ubuntu
-
-alias update="sudo apt-get -qq update"
-alias upgrade="sudo apt-get --qq update && sudo apt-get upgrade"
-alias install="sudo apt-get install"
-alias remove="sudo apt-get remove"
-alias search="apt-cache search"
